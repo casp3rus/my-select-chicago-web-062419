9ce87c98eb7318cf248ad456a2f143382a8facfb
Adrian Popan <adrian.popan@gmail.com>
2019-06-17 20:22:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,11 @@
 def my_select(collection)
  i = 0 
  new_collection = []
- 
- 
- 
+  while i < array.validates_length 
+    if yield(array[i])
+      new_collection << array[i]
+    end
+    i = i + 1
+  end
+  new_collection
 end
